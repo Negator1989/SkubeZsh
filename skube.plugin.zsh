c9b7db60b627ba849cd0aa986495e43f493c1ac5
flosi <federico.losi@facile.it>
2019-08-08 12:47:39 +0200

    added regex to list and watch action

--- skube.plugin.zsh
+++ skube.plugin.zsh
@@ -1,14 +1,28 @@
 #Alias skube
-alias skubewatch="kubectl get pod -w | awk '{if(NR%2 == 0){ print \"\\033[0;40m\"\$R\"\\033[0m\";}else{ print \$R;} }'"
-alias skubepod="kubectl get pod"
+
+#Exec
 skubeexecphp () { kubectl exec -ti $1 -cphp sh }
+skubeexecnginx () { kubectl exec -ti $1 -cnginx sh }
 skubexec () { kubectl exec -ti $1 sh }
-skubewatchbranch () { kubectl get pod -w | grep $1}
-skubelist () { kubectl get $1 | grep $2 | awk {'print $1'} }
+
+#Watch
+skubewatcharg () { kubectl get $1 -w | grep "$2" | awk '{if(NR%2 == 0){ print \"\\033[0;40m\"\$R\"\\033[0m\";}else{ print \$R;} }' }
+skubewatchpod () { kubectl get pod -w | grep "$1" | awk '{if(NR%2 == 0){ print \"\\033[0;40m\"\$R\"\\033[0m\";}else{ print \$R;} }'
+skubelistname () { kubectl get $1 | grep $2 | awk {'print $1'} }
+
+#delete 
 skubedelete () { kubectl delete $1  }
-skubeingress () { kubectl get ingress | grep $1 }
+
+#get
+skubepod () { kubectl get pod | grep "$1" }
+skubeingress () { kubectl get ingress | grep "$1" }
+skubedeployments () { kubectl get deployment | grep "$1" }
+
+#describe
+skubedescribepod () { kubectl describe pod $1 }
+#log
 skubelog () { kubectl logs $1 }
 skubelogphp () { kubectl logs $1 php }
 skubelognginx () { kubectl logs $1 nginx }
 skubelogmemcached () { kubectl logs $1 memcached }
-skubelogbranch () { kubetail $1 }
+skubelogarg () { kubetail $1 }
