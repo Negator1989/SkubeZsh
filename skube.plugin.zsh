bb3c6ef7485f735eff3a65ce6dbaa573e41d4dde
flosi <federico.losi@facile.it>
2019-08-09 12:29:33 +0200

    rework of some function

--- skube.plugin.zsh
+++ skube.plugin.zsh
@@ -1,22 +1,28 @@
 #Alias skube
-
+alias listname="awk {'print \$1'}"
 #Exec
-skubeexecphp () { kubectl exec -ti $1 -cphp sh }
-skubeexecnginx () { kubectl exec -ti $1 -cnginx sh }
+skubexecphp () { kubectl exec -ti $1 -cphp sh }
+skubexecnginx () { kubectl exec -ti $1 -cnginx sh }
 skubexec () { kubectl exec -ti $1 sh }
 
 #Watch
-skubewatcharg () { kubectl get $1 -w | grep "$2" | awk '{if(NR%2 == 0){ print \"\\033[0;40m\"\$R\"\\033[0m\";}else{ print \$R;} }' }
-skubewatchpod () { kubectl get pod -w | grep "$1" | awk '{if(NR%2 == 0){ print \"\\033[0;40m\"\$R\"\\033[0m\";}else{ print \$R;} }'
-skubelistname () { kubectl get $1 | grep $2 | awk {'print $1'} }
+skubewatchpod () { kubectl get pod -w | grep "$1"}
+skubelistargname () { kubectl get $1 | grep "$2" | awk {'print $1'} }
+skubewatcharg () { kubectl get $1 -w | grep "$2"}
 
 #delete 
-skubedelete () { kubectl delete $1  }
-
+skubedelpod () { kubectl delete pod $1 }
+skubedelarg () { kubectl delete pod $1 }
+skubedeldeployment () { kubectl delete deployment $1 }
+skubedelingress () { kubectl delete ingress $1 }
+skubedeletearglist () { 
+    kubectl delete $1 $(kubectl get $1 | grep $2 | awk {'print $1'} )
+}
 #get
 skubepod () { kubectl get pod | grep "$1" }
 skubeingress () { kubectl get ingress | grep "$1" }
 skubedeployments () { kubectl get deployment | grep "$1" }
+skubegetarg () { kubectl get $1 | grep "$2"}
 
 #describe
 skubedescribepod () { kubectl describe pod $1 }
